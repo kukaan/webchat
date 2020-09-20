80966277774a01a8067a3de7fa2751b2e5306f1c
kukaan <58886775+kukaan@users.noreply.github.com>
2020-09-20 11:36:58 +0300

    add messages, users. refactor

---
+++ routes.py
@@ -0,0 +1,41 @@
+from app import app
+from flask import render_template, redirect, request
+import messages, users
+
+@app.route("/")
+def index():
+    list = messages.get_list()
+    return render_template("index.html", count=len(list), messages=list)
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    if messages.send(content):
+        return redirect("/")
+    else:
+        return render_template("error.html",message="Viestin lähetys ei onnistunut")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username,password):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Väärä tunnus tai salasana")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.register(username,password):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Rekisteröinti ei onnistunut")
+
